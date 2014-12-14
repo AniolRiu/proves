839321627bfa5d549d50e795992e50fc0fe49646
AniolRiu <artic.vb@gmail.com>
2014-12-14 20:33:00 +0100

    cssc
    sccs

--- js/func.js
+++ js/func.js
@@ -26,9 +26,7 @@ function carregaImatge() {
 }
 
 function initApp() {
-	setInterval(function(){
-					carregaImatge();
-				}, 3000);
+	setInterval(carregaImatge, 30000);
 	carregaImatge();
 	//if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
 	
