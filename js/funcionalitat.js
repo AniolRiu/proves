72d2884d974e1f88e3b10cd666e1ac2e59c2a04c
AniolRiu <artic.vb@gmail.com>
2014-12-16 20:43:10 +0100

    d
    d

--- js/funcionalitat.js
+++ js/funcionalitat.js
@@ -17,12 +17,10 @@ function onLoad() {
         
 var ad_units = {
 	ios : {
-		banner: 'ca-app-pub-6869992474017983/4806197152',
-		interstitial: 'ca-app-pub-6869992474017983/7563979554'
+		banner: 'ca-app-pub-5785179440070320/2064822897'
 	},
 	android : {
-		banner: 'ca-app-pub-5785179440070320/4757924099',
-		interstitial: 'ca-app-pub-6869992474017983/1657046752'
+		banner: 'ca-app-pub-5785179440070320/7600312495'
 	}
 };
 var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
@@ -55,20 +53,6 @@ function initAd(){
 }
 // optional, in case respond to events or handle error
 function registerAdEvents() {
-	/* deprecated
-	document.addEventListener('onBannerFailedToReceive', function(data){ alert('error: ' + data.error + ', reason: ' + data.reason); });
-	document.addEventListener('onBannerReceive', function(){});
-	document.addEventListener('onBannerPresent', function(){});
-	document.addEventListener('onBannerLeaveApp', function(){});
-	document.addEventListener('onBannerDismiss', function(){});
-	
-	document.addEventListener('onInterstitialFailedToReceive', function(data){ alert('error: ' + data.error + ', reason: ' + data.reason); });
-	document.addEventListener('onInterstitialReceive', function(){});
-	document.addEventListener('onInterstitialPresent', function(){});
-	document.addEventListener('onInterstitialLeaveApp', function(){});
-	document.addEventListener('onInterstitialDismiss', function(){});
-	*/
-	
 	// new events, with variable to differentiate: adNetwork, adType, adEvent
 	document.addEventListener('onAdFailLoad', function(data){ 
 		alert('error: ' + data.error + 
@@ -82,43 +66,12 @@ function registerAdEvents() {
 	document.addEventListener('onAdLeaveApp', function(data){});
 	document.addEventListener('onAdDismiss', function(data){});
 }
-
-// click button to call following functions
-/*function getSelectedAdSize() {
-	var i = document.getElementById("adSize").selectedIndex;
-	var items = document.getElementById("adSize").options;
-	return items[i].value;
-}
-function getSelectedPosition() {
-	var i = document.getElementById("adPosition").selectedIndex;
-	var items = document.getElementById("adPosition").options;
-	return parseInt( items[i].value );
-}*/
 function createSelectedBanner() {
 	var overlap = document.getElementById('overlap').checked;
 	var offsetTopBar = document.getElementById('offsetTopBar').checked;
-	AdMob.createBanner( {adId:admobid.banner/*, overlap:false, offsetTopBar:false*/} );
+	AdMob.createBanner( {adId:admobid.banner} );
 }
-/*function createBannerOfGivenSize() {
-	var w = document.getElementById('w').value;
-	var h = document.getElementById('h').value;
-	
-	AdMob.createBanner( {adId:admobid.banner,
-					   adSize:'CUSTOM', width:w, height:h,
-					   position:getSelectedPosition()} );
-}
-function showBannerAtSelectedPosition() {
-	AdMob.showBanner( getSelectedPosition() );
-}
-function showBannerAtGivenXY() {
-	var x = document.getElementById('x').value;
-	var y = document.getElementById('y').value;
-	AdMob.showBannerAtXY(x, y);
-}
-function prepareInterstitial() {
-	var autoshow = document.getElementById('autoshow').checked;
-	AdMob.prepareInterstitial({adId:admobid.interstitial, autoShow:autoshow});
-}*/
+
 function onResize(){
 	carregaImatge();
 	var s = document.getElementById('sizeinfo');
