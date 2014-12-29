5c5cfc56684e40721b84e83dcdd89b54e874577f
AniolRiu <artic.vb@gmail.com>
2014-12-29 19:39:18 +0100

    fy8o8myfni
    tgrefrw

--- js/funcionalitat_canvas.js
+++ js/funcionalitat_canvas.js
@@ -3,9 +3,6 @@ $(document).ready(function() {
 	var canv = $("#canvas");
 	var context = can.getContext("2d");
 	
-	var clickX = new Array();
-	var clickY = new Array();
-	var clickDrag = new Array();
 	var paint;
 	
 	var colorBlack = "#000";
@@ -15,15 +12,19 @@ $(document).ready(function() {
 	var colorWhite = "#FFF";
 	
 	var curColor = colorBlack;	// Guarda el color del llapis 
-	var clickColor = new Array();
 	
 	var curSize = 20; // Guarda la mida del llapis 
-	var clickSize = new Array();
 	
 	$(document).on("pageshow","#fes_dibuix", function() {
 		inicia_canvas();
 		$('#div_punta .ui-slider-track .ui-btn.ui-slider-handle').css({"height":curSize, "width":curSize});
 		$('#div_punta .ui-btn-up-c, .ui-btn-hover-c').css({"background":curColor});
+		
+		$('#color_negre').css({"background":colorBlack});
+		$('#color_blau').css({"background":colorBlue});
+		$('#color_vermell').css({"background":colorRed});
+		$('#color_verd').css({"background":colorGreen});
+		$('#color_blanc').css({"background":colorWhite});
 		
 		$('#color_blanc').bind('click',function(){
 			curColor = colorWhite;
@@ -54,6 +55,38 @@ $(document).ready(function() {
 			curSize = $('#slider').val();
 			$('#div_punta .ui-slider-track .ui-btn.ui-slider-handle').css({"height":curSize, "width":curSize});
 		});
+		
+		//---- BOTONS----
+		//---------------
+		$('#clear').bind('click',function(){
+			netejaCanvas();
+		});
+		
+		$('#save').bind('click',function(){
+			var img=canvas.toDataURL("image/jpg");
+			//$canvas.mouseup();
+			$('#solucio_detall').append('\
+				<div class="imatge">\
+					<p class="index_figura"></p>\
+					<div class="contenidor_imatge">\
+						<img class="imatge_solucio" src="' + img + '"/>\
+						<a data-role="button" class="eliminar_imatge"  data-icon="delete" data-iconpos="notext" class="ui-btn-right"></a>\
+					</div>\
+				</div>'
+			);
+			$(".eliminar_imatge").button();
+			netejaCanvas();
+			$.mobile.changePage( "#aporta_solucio", { transition: "slide"} );
+		});
+		
+		$('#cancel').bind('click',function(){
+			$.mobile.changePage( "#aporta_solucio", { transition: "slide"} );
+			netejaCanvas();
+		});
+		//--------------
+		//--------------
+		
+		window.onresize = inicia_canvas;
 	});
 	
 	function inicia_canvas() {
@@ -66,29 +99,7 @@ $(document).ready(function() {
 	function netejaCanvas() {
 		context.fillStyle=colorWhite;
 	  	context.fillRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-		
-		clickX = [];
-		clickY = [];
-		clickDrag = [];
-		clickColor = [];
-		clickSize = [];
 	}
-	
-	$('#div_punta .ui-slider-track .ui-btn.ui-slider-handle').css({"height":curSize, "width":curSize});
-	$('#color_negre').css({"background":colorBlack});
-	$('#color_blau').css({"background":colorBlue});
-	$('#color_vermell').css({"background":colorRed});
-	$('#color_verd').css({"background":colorGreen});
-	$('#color_blanc').css({"background":colorWhite});
-	
-	/*function addClick(x, y, dragging){
-	  clickX.push(x);
-	  clickY.push(y);
-	  clickDrag.push(dragging);
-	  clickColor.push(curColor);  
-	  clickSize.push(curSize);
-	}*/
-	
 	
 	var click_antic_X;
 	var click_antic_Y;
@@ -110,30 +121,9 @@ $(document).ready(function() {
 		click_antic_Y = click_Y;
 	}
 	
-	/*function redraw(){
-	  context.fillStyle=colorWhite;
-	  context.fillRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-	  context.lineJoin = "round";
-				
-	  for(var i=0; i < clickX.length; i++) {		
-		context.beginPath();
-		if(clickDrag[i] && i){
-		  context.moveTo(clickX[i-1], clickY[i-1]);
-		 }else{
-		   context.moveTo(clickX[i]-1, clickY[i]);
-		 }
-		 context.lineTo(clickX[i], clickY[i]);
-		 context.closePath();
-		 context.strokeStyle = clickColor[i];
-		 context.lineWidth = clickSize[i];
-		 context.stroke();
-	  }
-	}*/
-	
 	// inici_dibuix ---------
 	/*canv.mousedown(function(e){
 		paint = true;
-	  	//addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
 	  	draw(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, false);
 	});*/
 	
@@ -147,7 +137,6 @@ $(document).ready(function() {
 	// fent_dibuix ---------
 	/*canv.mousemove(function(e){
 	  	if(paint){
-			//addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
 			draw(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
 	  	}
 	});*/
@@ -157,7 +146,6 @@ $(document).ready(function() {
 		if(paint){
 			toc = e.changedTouches[0];
 			draw(toc.pageX - this.offsetLeft, toc.pageY - this.offsetTop, true);
-			//redraw();
 	  	}
 	}, false);
 	
@@ -183,38 +171,5 @@ $(document).ready(function() {
 	}, false);
 	
 	//-------------------------
-	
-	$('#cancel').bind('click',function(){
-		curColor = colorGreen;
-		$('#div_punta .ui-btn-up-c, .ui-btn-hover-c').css({"background":curColor});
-	});
-	
-	$('#clear').bind('click',function(){
-		netejaCanvas();
-	});
-	
-	$('#save').bind('click',function(){
-		var img=canvas.toDataURL("image/jpg");
-		//$canvas.mouseup();
-		$('#solucio_detall').append('\
-			<div class="imatge">\
-				<p class="index_figura"></p>\
-				<div class="contenidor_imatge">\
-					<img class="imatge_solucio" src="' + img + '"/>\
-					<a data-role="button" class="eliminar_imatge"  data-icon="delete" data-iconpos="notext" class="ui-btn-right"></a>\
-				</div>\
-			</div>'
-		);
-		$(".eliminar_imatge").button();
-		netejaCanvas();
-		$.mobile.changePage( "#aporta_solucio", { transition: "slide"} );
-	});
-	
-	$('#cancel').bind('click',function(){
-		$.mobile.changePage( "#aporta_solucio", { transition: "slide"} );
-		netejaCanvas();
-	});
-	
-	window.onresize = inicia_canvas;
 });
 
