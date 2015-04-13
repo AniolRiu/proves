cc116481fdbd53954ca3d2bc552f170802a6b22c
AniolRiu <artic.vb@gmail.com>
2015-04-13 03:06:04 +0200

    xwe
    ex

--- js/funcionalitat.js
+++ js/funcionalitat.js
@@ -1,180 +1,168 @@
-var adresa;
-var h = ['res/ops_h.png'];
-var v = ['res/ops_v.png'];
-var orientacio = (window.screen.availWidth > window.screen.availHeight ? "h" : "v" );
-var num_imatges_h=h.length;
-var num_imatges_v=v.length;
-var periode = 15;
-var interval;
+url_autenticacio = "http://queprefereixes.tk/app_connection/autenticacio.php";
+url_pregunta_random = "http://queprefereixes.tk/app_connection/get_random_question.php";
+var jsoncb = "?jsoncallback=?";
 
-$(document).ready(function() {
-	carregaImatge();
-	$("#div_missatge").hide();
-	interval=setInterval(function() {carregaImatge();}, periode * 1000);
-	$('html').click(function() {carregaImatge();});
-	descarregaLlista();
-	
-	
-	if(( /(ipad|iphone|ipod|android)/i.test(navigator.userAgent) )) {
-		document.addEventListener('deviceready', initApp, false);
-	} else {
-		document.addEventListener('keydown', function(event) {
-			if(event.keyCode == 37) {
-				desaccelera();
-			}
-			else if(event.keyCode == 39) {
-				accelera();
-			}
-		});
-		initApp();
-	}
-});
+// Wait for device API libraries to load
+window.onload = onDeviceReady;
 
-function desaccelera() {
-	/*
-	 * El periode minim son 5 segons
-	 * De 5 a 15 augmenta de 5 en 5
-	 * de 15 a 60 augmenta de 15 en 15
-	 * a partir de 60 augmenta de 60 en 60
-	 */
-	var missatge;
-	if (periode == 5 ) {
-		missatge = "Minimum refresh time is 5 secs";
-	}
-	else if (periode < 16) {
-		periode -= 5;
-		missatge = "Refresh time: " + periode + " secs";
-	}
-	else if (periode < 61) {
-		periode -= 15;
-		missatge = "Refresh time: " + periode + " secs";
-	}
-	else {
-		periode -= 60;
-		missatge = "Refresh time: " + periode / 60 + " mins";
-	}
-	$("#missatge").html(missatge);
-	$("#div_missatge").show();
-	setTimeout(function() {$("#div_missatge").hide();},5000);
-	clearInterval(interval);
-	interval = setInterval(function() {carregaImatge();}, periode * 1000);
-}
-
-function accelera() {
-	/*
-	 * El periode minim son 5 segons
-	 * De 5 a 15 augmenta de 5 en 5
-	 * de 15 a 60 augmenta de 15 en 15
-	 * a partir de 60 augmenta de 60 en 60
-	 */
-	var missatge;
-	if (periode < 14) {
-		periode += 5;
-		missatge = "Refresh time: " + periode + " secs";
-	}
-	else if (periode < 59) {
-		periode += 15;
-		missatge = "Refresh time: " + periode + " secs";
-	}
-	else {
-		periode += 60;
-		missatge = "Refresh time: " + periode / 60 + " mins";
-	}
-	$("#missatge").html(missatge);
-	$("#div_missatge").show();
-	setTimeout(function() {$("#div_missatge").hide();},5000);
-	clearInterval(interval);
-	interval = setInterval(function() {carregaImatge();}, periode * 1000);
-}
-
-function getAdresa(num) {
-	adresa=(orientacio == "h" ? h[num] : v[num]);
-}
-
-function descarregaLlista() {
-	$.getJSON(
-		"http://randomframe.tk/repo.php?jsoncallback=?",
-		{
-			num:"l"	//llista
-		}, 
+function onDeviceReady() {
+	//document.getElementById("boto_login").onclick = autenticacio; // AIXO VA AL EVENT PAGE_AUTENTICACIO_ON_SHOW
+	$.getJSON( 
+		url_pregunta_random.concat(jsoncb), 
+		{}, 
 		function(resposta) {
 			console.log(resposta);
-			h=resposta.horitzontals;
-			v=resposta.verticals;
-			num_imatges_h=h.length; 
-			num_imatges_v=v.length;
+			if (resposta.success == 1) {
+				$('#pregunta').text(resposta.Pregunta);
+				$("#resposta1").prev('span').find('span.ui-btn-text').text(resposta.Resposta1);
+				$("#resposta2").prev('span').find('span.ui-btn-text').text(resposta.Resposta2);
+				
+				jQuery(function ($) {
+            /**
+             * A jQuery plugin that loads data from HTML tables, Google Sheets and data arrays and draws charts using Google Charts.
+             *
+             * Using HTML Tables
+             * HTML tables can help make the chart data accessible.
+             * You can either display the table with the chart or accessibly hide the table
+             *
+             * Suggested HTML Table setup
+             * Create an HTML table with a caption and 'th' elements in the first row
+             * For each 'th' element apply one of the following
+             * 'data-type' attribute: 'string' 'number' 'boolean' 'date' 'datetime' 'timeofday'
+             * or 'data-role' attribute:  'tooltip','annotation'
+             * The caption element's text is used as a title for the chart by default.
+             *
+             * Apply the jQuery Chartinator plugin to the chart canvas(es)
+             * or select the table(s) and Chartinator will insert a new chart canvas(es) after the table
+             * or create js data arrays
+             * See examples below and the readme file for more info
+             */
+
+            
+            //  Pie Chart Example
+            var chart2 = $('#pieChart').chartinator({
+
+                // Custom Options ------------------------------------------------------
+                // Note: This example appends data from a data array
+                // to the data extracted from an HTML table
+                // Google Charts does not support custom tooltips or annotations on Pie Charts
+
+                // Append the following rows of data to the data extracted from the table
+                rows: [
+                    ['France', 5],
+                    ['Mexico', 2]],
+
+                // The chart type - String
+                // Derived from the Google Charts visualization class name
+                // Default: 'BarChart'
+                // Use TitleCase names. eg. BarChart, PieChart, ColumnChart, Calendar, GeoChart, Table.
+                // See Google Charts Gallery for a complete list of Chart types
+                // https://developers.google.com/chart/interactive/docs/gallery
+                chartType: 'PieChart',
+
+                // The chart aspect ratio custom option - width/height
+                // Used to calculate the chart dimensions relative to the width or height
+                // this is overridden if the Google Chart's height and width options have values
+                // Suggested value: 1.25
+                // Default: false - not used
+                chartAspectRatio: 1.25,
+
+                // Google Pie Chart Options
+                pieChart: {
+
+                    // Width of chart in pixels - Number
+                    // Default: automatic (unspecified)
+                    width: null,
+
+                    // Height of chart in pixels - Number
+                    // Default: automatic (unspecified)
+                    //height: 400,
+
+                    chartArea: {
+                        left: "25%",
+                        top: "0%",
+                        width: "50%",
+                        height: "50%"
+                    },
+
+                    // The font size in pixels - Number
+                    // Or use css selectors as keywords to assign font sizes from the page
+                    // For example: 'body'
+                    // Default: false - Use Google Charts defaults
+                    fontSize: 'body',
+
+                    // Font-family name - String
+                    // Default: 'Arial'
+                    //fontName: 'Roboto',
+
+                    // Chart Title - String
+                    // Default: Table caption.
+                    //title: 'Pie Chart Sample',
+
+                    titleTextStyle: {
+
+                        // The font size in pixels - Number
+                        // Or use css selectors as keywords to assign font sizes from the page
+                        // For example: 'body'
+                        // Default: false - Use Google Charts defaults
+                        fontSize: 'h3'
+                    },
+                    legend: {
+
+                        // Legend position - String
+                        // Options: bottom, top, left, right, in, none.
+                        // Default: right
+                        position: 'none'
+                    },
+
+                    // Array of colours
+                    colors: ['#94ac27', '#3691ff', '#e248b3', '#f58327', '#bf5cff'],
+
+                    // Make chart 3D - Boolean
+                    // Default: false.
+                    is3D: true,
+
+                    tooltip: {
+
+                        // Shows tooltip with values on hover - String
+                        // Options: focus, none.
+                        // Default: focus
+                        trigger: 'focus'
+                    }
+                }
+            });
+
+        });
+				
+			} else {
+				//TODO: Deal with
+			}
 		}
 	);
 }
 
-function carregaImatge() {
-	var num_imatges = (orientacio == "h" ? num_imatges_h : num_imatges_v);
-	getAdresa(Math.floor(Math.random() * num_imatges));
-	document.body.style.backgroundImage="url(" + adresa + ")";
-}
-
-function createSelectedBanner() {
-	AdMob.createBanner( {adId:admobid.banner} );
-}
-		
-var ad_units = {
-	ios : {
-		banner: 'ca-app-pub-5785179440070320/2064822897'
-	},
-	android : {
-		banner: 'ca-app-pub-5785179440070320/7600312495'
-	}
-};
-var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
-
-function initApp() {
-	// Les dues comandes següents s'han de cridar un cop s'ha fet el fire de deviceready
-	document.addEventListener("volumedownbutton", desaccelera, false);
-	document.addEventListener("volumeupbutton", accelera, false);
-	
-	// A partir daki, publicitat
-	if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
-	initAd();
-	// display the banner at startup
-	
-	window.onclick = carregaImatge;
-	createSelectedBanner();
-}
-
-function initAd(){
-	var defaultOptions = {
-		bannerId: admobid.banner,
-		adSize: 'SMART_BANNER',
-		position: AdMob.AD_POSITION.BOTTOM_CENTER,
-		offsetTopBar: false, // avoid overlapped by status bar, for iOS7+
-		bgColor: 'black', // color name, or '#RRGGBB'
-		isTesting: false, // set to true, to receiving test ad for testing purpose
-		autoShow: true // auto show interstitial ad when loaded, set to false if prepare/show
-	};
-	AdMob.setOptions( defaultOptions );
-	registerAdEvents();
-}
-// optional, in case respond to events or handle error
-function registerAdEvents() {
-	// new events, with variable to differentiate: adNetwork, adType, adEvent
-	document.addEventListener('onAdFailLoad', function(data){ 
-		alert('error: ' + data.error + 
-				', reason: ' + data.reason + 
-				', adNetwork:' + data.adNetwork + 
-				', adType:' + data.adType + 
-				', adEvent:' + data.adEvent); // adType: 'banner' or 'interstitial'
-	});
-	document.addEventListener('onAdLoaded', function(data){});
-	document.addEventListener('onAdPresent', function(data){});
-	document.addEventListener('onAdLeaveApp', function(data){});
-	document.addEventListener('onAdDismiss', function(data){});
-}
-
-function onResize() {
-	var or_antic = orientacio;
-	orientacio = ($( window ).width() > $( window ).height() ? "h" : "v" );
-	if (or_antic != orientacio) {carregaImatge();}
-	
-	//var s = document.getElementById('sizeinfo');
-	//s.innerHTML = "web view: " + window.innerWidth + " x " + window.innerHeight;
+function autenticacio() {
+	$.getJSON( 
+		url_autenticacio.concat(jsoncb), 
+		{
+			usuari:document.getElementById("usuari").value, 
+			password:document.getElementById("password").value
+		}, 
+		function(resposta) {
+			console.log(resposta);
+			if (resposta.success == 1) {
+				// Autenticació correcta
+				id_usuari_global = resposta.id_usuari;
+				password_global = resposta.password;
+				$.mobile.changePage($('#selecciona_exercici')/*, { transition: "flip"}*/ );
+				//$("#autenticacio").trigger("pagecreate");
+				//window.location.replace = "#selecciona_exercici";
+				carregaEditorials();
+			} else {
+				$('#capsa_login').shake();
+				$('#error_login').html("<span style='color:#cc0000'>Error:</span> Nombre de usuario o contraseña incorrectos.");
+			}
+		}
+	);
+	return false;
 }
